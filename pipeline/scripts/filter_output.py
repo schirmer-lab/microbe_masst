ae11396f42a9eb11ace9e6c02be190b6d1c30749
Anton <ge54kog@mytum.de>
2024-12-06 14:31:52 +0100

    Change input to mutually exclusive one, either --species_level, --genus or --species. Fix logic for species extracting

--- pipeline/scripts/filter_output.py
+++ pipeline/scripts/filter_output.py
@@ -2,11 +2,14 @@ import pandas as pd
 import argparse
 
 parser = argparse.ArgumentParser(description="Filter for output of MicrobeMASST")
-parser.add_argument('-i','--input', type=str, required=True, help='Path to the input TSV file')
-parser.add_argument('-o','--output', type=str, required=True, help='Path to the output TSV file')
-parser.add_argument('--species_level', default=False, action=argparse.BooleanOptionalAction, help='Filter by species level')
-parser.add_argument('--genus', type=str, help='Genus to filter by')
-parser.add_argument('--species', type=str, help='Species to filter by')
+
+group = parser.add_mutually_exclusive_group(required=True) # only allow --species_level, --genus or --species
+
+parser.add_argument('-i', '--input', type=str, required=True, help='Path to the input TSV file')
+parser.add_argument('-o', '--output', type=str, required=True, help='Path to the directory, name is automatically generated!')
+group.add_argument('--species_level', default=False, action="store_true", help='Filter by species level')
+group.add_argument('--genus', type=str, help='Genus to filter by')
+group.add_argument('--species', type=str, help='Species to filter by')
 
 args = parser.parse_args()
 
@@ -16,9 +19,11 @@ splits = df["Taxaname_file"].str.split(" ")
 if args.species_level:
     filtered_df = df[splits.str.len() > 1]
 elif args.genus:
-    filtered_df = df[splits.str[0] == args.genus]
+    filtered_df = df[splits.str[0].str.lower() == args.genus.lower()]
 elif args.species:
-    filtered_df = df[' '.join([splits.str[0],splits.str[1]]) == args.species] #TODO: Check if this works
+    valid_splits = splits[splits.str.len() > 1]
+    combined_splits = valid_splits.str[0] + " " + valid_splits.str[1]
+    filtered_df = df.loc[valid_splits.index[combined_splits.str.lower() == args.species.lower()]]
 else:
     filtered_df = df
 
