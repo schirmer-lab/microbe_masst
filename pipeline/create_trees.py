5a0c88bf2b0b487c8ef6c2fbc7ce4aa374d52e45
meritback <merit.back@online.de>
2024-12-11 16:18:56 +0100

    create annotation file for tree on phylum ll

--- pipeline/create_trees.py
+++ pipeline/create_trees.py
@@ -31,18 +31,51 @@ def get_phylum_name(taxid):
     
     return None
 
-# EITHER: Assign phylum names to leaf nodes
+# # EITHER: Assign phylum names to leaf nodes
+# # Assign phylum names to leaf nodes
+# use_phylum = False
+# if use_phylum:
+#     for node in tree.traverse():
+#         if node.is_leaf():  # Rename only leaf nodes
+#             node.name = get_phylum_name(int(node.name))
+# else:
+#     #OR: Assign scientific names to internal nodes
+#     for node in tree.traverse():
+#         if node.is_leaf():  # Rename only leaf nodes
+#             node.name = f"{ncbi.get_taxid_translator([int(node.name)])}"
+import random
+
 # Assign phylum names to leaf nodes
-use_phylum = False
-if use_phylum:
-    for node in tree.traverse():
-        if node.is_leaf():  # Rename only leaf nodes
-            node.name = get_phylum_name(int(node.name))
-else:
-    #OR: Assign scientific names to internal nodes
-    for node in tree.traverse():
-        if node.is_leaf():  # Rename only leaf nodes
-            node.name = f"{ncbi.get_taxid_translator([int(node.name)])}"
+phylum_annotations = {}
+for node in tree.traverse():
+    if node.is_leaf():  # Rename only leaf nodes
+        taxid = int(node.name)
+        phylum_name = get_phylum_name(taxid)
+        node.name = ncbi.get_taxid_translator([taxid])[taxid]
+        if phylum_name:
+            phylum_annotations[node.name] = phylum_name
+
+# Generate random colors for each phylum
+phylum_colors = {}
+for phylum in set(phylum_annotations.values()):
+    phylum_colors[phylum] = "#{:06x}".format(random.randint(0, 0xFFFFFF))
+
+# Create the iTOL annotation file
+with open("itol_annotations.txt", "w") as f:
+    f.write("DATASET_COLORSTRIP\n")
+    f.write("SEPARATOR TAB\n")
+    f.write("DATASET_LABEL\tPhylum Annotations\n")
+    f.write("COLOR\t#ff0000\n")
+    f.write("LEGEND_TITLE\tPhylum\n")
+    f.write("LEGEND_SHAPES\t{}\n".format("\t".join(["1"] * len(phylum_colors))))
+    f.write("LEGEND_COLORS\t{}\n".format("\t".join(phylum_colors.values())))
+    f.write("LEGEND_LABELS\t{}\n".format("\t".join(phylum_colors.keys())))
+    f.write("BORDER_WIDTH\t1\n")
+    f.write("BORDER_COLOR\t#000000\n")
+    f.write("DATA\n")
+    for leaf_name, phylum_name in phylum_annotations.items():
+        f.write("{}\t{}\n".format(leaf_name, phylum_colors[phylum_name]))
+
 # Visualize the tree
 print(tree)
 #tree.show()
