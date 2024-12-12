b597d1f8f3749725ead4db46188c37fd299fcd14
Anton <ge54kog@mytum.de>
2024-12-12 11:17:02 +0100

    refined output filtering to include rank search with NCBI url to ensure given rank in Taxaname_file column

--- pipeline/filter_output.py
+++ pipeline/filter_output.py
@@ -1,36 +1,86 @@
 import pandas as pd
 import argparse
+import requests
 
 
-# ArgParser
-parser = argparse.ArgumentParser(description="Filter for output of MicrobeMASST")
+def setup_argparser():
+    parser = argparse.ArgumentParser(description="Filter for output of MicrobeMASST")
 
-group = parser.add_mutually_exclusive_group(required=True) # only allow --species_level, --genus or --species
+    group = parser.add_mutually_exclusive_group(required=True)  # only allow --species_level, --genus or --species
 
-parser.add_argument('-i', '--input', type=str, required=True, help='Path to the input TSV file')
-parser.add_argument('-o', '--output', type=str, required=True, help='Path to the directory, name is automatically generated!')
-# choose 1 of these 3
-group.add_argument('--species_level', default=False, action="store_true", help='Filter by species level')
-group.add_argument('--genus', type=str, help='Genus to filter by')
-group.add_argument('--species', type=str, help='Species to filter by')
+    parser.add_argument('-i', '--input', type=str, required=True,
+                        help='Path to the input TSV file')
+    parser.add_argument('-o', '--output', type=str, required=True,
+                        help='Path to the directory, name is automatically generated!')
+    group.add_argument('--species_level', default=False, action="store_true",
+                       help='Filter by species level')
+    group.add_argument('--species', type=str,
+                       help='Species to filter by')
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-# tsv to pandas df
-df = pd.read_csv(args.input, sep='\t')
+    return args
 
-# filter for either species level, genus query or species query
-splits = df["Taxaname_file"].str.split(" ")
-if args.species_level:
-    filtered_df = df[splits.str.len() > 1]
-elif args.genus:
-    filtered_df = df[splits.str[0].str.lower() == args.genus.lower()]
-elif args.species:
-    valid_splits = splits[splits.str.len() > 1]
-    combined_splits = valid_splits.str[0] + " " + valid_splits.str[1]
-    filtered_df = df.loc[valid_splits.index[combined_splits.str.lower() == args.species.lower()]]
-else:
-    filtered_df = df
 
-# save filtered df as tsv
-filtered_df.to_csv(f'{args.output}/filtered_{args.input.split("/")[-1]}', sep='\t')
+def get_taxonomy_rank(tax_ids):
+    # check for valid tax IDS, if none return NAs of length input
+    valid_tax_ids = [tax_id for tax_id in tax_ids if tax_id.isdigit()]
+    if not valid_tax_ids:
+        return [pd.NA] * len(tax_ids)
+
+    # URL and input for NCBI taxa information
+    url = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi"
+    params = {
+        "db": "taxonomy",
+        "id": ",".join(map(str, valid_tax_ids)),  # Join TaxIDs with commas
+        "retmode": "json",
+    }
+
+    # output and its jsonification
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    # extract ranks from output, incorporate invalids accordingly
+    ranks = []
+    for tax_id in tax_ids:
+        if tax_id.isdigit():
+            rank = data['result'][str(tax_id)]['rank']
+            ranks.append(rank)
+        else:
+            ranks.append(pd.NA)
+
+    return ranks
+
+
+def batch_query(tax_ids, batch_size=10):
+    for i in range(0, len(tax_ids), batch_size):
+        yield tax_ids[i:i+batch_size]
+
+if __name__ == "__main__":
+    # retrieve arguments from argparser
+    args = setup_argparser()
+
+    # tsv to pandas df
+    df = pd.read_csv(args.input, sep='\t')
+
+    # get ranks fpr each taxonomy ID
+    ranks = []
+    for batch in batch_query(df["Taxa_NCBI"], 50):
+        ranks.extend(get_taxonomy_rank(batch))
+
+    # append rank column to df
+    df["ranks"] = ranks
+
+    # filter for either species level, genus query or species query
+    if args.species_level:
+        filtered_df = df[df["ranks"].isin(["species", ""])]
+    elif args.species:
+        splits = df["Taxaname_file"].str.split(" ")
+        valid_splits = splits[splits.str.len() > 1]
+        combined_splits = valid_splits.str[0] + " " + valid_splits.str[1]
+        filtered_df = df[df["ranks"].isin(["species", ""]) & (combined_splits.str.lower() == args.species.lower())]
+    else:
+        filtered_df = df
+
+    # save filtered df as tsv
+    filtered_df.to_csv(f'{args.output}/filtered_{args.input.split("/")[-1]}', sep='\t')
