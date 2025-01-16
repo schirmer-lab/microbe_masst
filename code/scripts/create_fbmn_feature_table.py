6564092db8b76ce7fd4ea06a3dd42b520db4389a
Anton <ge54kog@mytum.de>
2025-01-16 11:02:45 +0100

    rename columns of combined feature table for FBMN to mzXML file names, and fix tsv reading, qouting needed to be adjusted

--- code/scripts/create_fbmn_feature_table.py
+++ code/scripts/create_fbmn_feature_table.py
@@ -1,11 +1,17 @@
 import pandas as pd
+import csv
 
-exp = "files/pos_expression.tsv"
-viewer_table = "files/pos_viewerTable.tsv"
+pos_neg = "neg"
 
-exp = pd.read_csv(exp, sep="\t")
-viewer_table = pd.read_csv(viewer_table, sep="\t")
+# set paths according to positive or negative value
+exp = f"files/{pos_neg}_expression.tsv"
+viewer_table = f"files/{pos_neg}_viewerTable.tsv"
 
+# read files
+exp = pd.read_csv(exp, sep="\t", index_col=0)
+viewer_table = pd.read_csv(viewer_table, sep="\t", quoting=csv.QUOTE_NONE)
+
+# rename columns of viewer table to match ID of expression table and remove unnecessary
 viewer_table.rename(columns={
     "General|All|ID": "feature",
     "General|All|rtmed": "rtmed",
@@ -17,8 +23,29 @@ viewer_table.rename(columns={
     "General|Extended|npeaks": "npeaks"
 }, inplace=True)
 
+# remove redundant columns of the viewer table
 viewer_table = viewer_table[["feature", "mzmed", "mzmin", "mzmax", "rtmed", "rtmin", "rtmax", "npeaks"]]
 
-complete_df = pd.concat([viewer_table, exp])
+# set index for viewer table
+viewer_table.set_index("feature", inplace=True)
 
-complete_df.to_csv("files/FBMN/pos_feature_table.tsv", sep="\t")
+# Create a boolean array indicating which columns contain the string "Email"
+cols_to_drop = exp.columns[exp.columns.str.contains('QC')]
+
+# Drop the columns containing the string "Email"
+exp.drop(cols_to_drop, axis=1, inplace=True)
+
+# convert columns names of expression table to mzXML file names
+new_col_names = []
+for col_name in exp.columns:
+    if col_name != "feature":
+        new_col_names.append(f"241008_M274_01_HILIC_neg_{col_name}.mzXML")
+    else:
+        new_col_names.append(col_name)
+
+exp.columns = new_col_names
+
+# concat both dfs
+complete_df = pd.concat([viewer_table, exp], axis=1)
+
+complete_df.to_csv(f"files/FBMN/{pos_neg}_feature_table.tsv", sep="\t")
